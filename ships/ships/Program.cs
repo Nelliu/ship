6fd734dd89b207ee77054bdd76b6d9751c13ee74
matyas.kloub <matyas.kloub@seznam.cz>
2018-09-18 10:21:22 +0200

    v0.0.1

---
+++ ships/ships/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            retab Retabc = new retab(); 
+            List<Point> Position = new List<Point>();
+            int howm = 0;
+
+                                                                // map 
+            for (int sloupec = 0; sloupec < 10; sloupec++)
+            {
+                Console.WriteLine();
+                for (int radek = 0; radek < 10; radek++)
+                {
+                    int all = sloupec + radek;
+                    
+                    if (all % 2 == 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+                        
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                    }
+                    Position[sloupec].X = sloupec;
+                    Position[radek].Y = radek;
+                    Console.Write("   ");
+                }
+                
+            }
+
+
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ships/ships/retab.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ships
+{
+    class retab
+    {
+        List<string> abc = new List<string>();
+       
+
+        public retab()
+        {
+            abc.Add("A");
+            abc.Add("B");
+            abc.Add("C");
+            abc.Add("D");
+            abc.Add("E");
+            abc.Add("F");
+            abc.Add("G");
+            abc.Add("H");
+            abc.Add("I");
+            abc.Add("J");
+
+        }
+
+        public List<string> Retab()
+        {
+            return abc;
+        }
+    }
+}
